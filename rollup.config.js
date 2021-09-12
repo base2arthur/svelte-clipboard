75affc472c8f413ece81a6874ca320d57b63f807
Arthur Marinis <a.marinis@base2services.com>
2021-09-12 12:20:57 +1000

    Fuirst Init

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import svelte from "rollup-plugin-svelte";
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from "./package.json";
 
-const input = "src/Index.svelte";
+const input = "src/Clipboard.svelte";
 
 export default [
   {
     input,
-    output: { file: pkg.main, format: "umd", name: "Component" },
+    output: { file: pkg.main, format: "umd", name: "Clipboard" },
     plugins: [svelte(), resolve()],
   },
   {
---
+++ src/Clipboard.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {  createEventDispatcher } from "svelte";
+  export let value
+  export let className
+  export {className as class}
+  const dispatch = createEventDispatcher();
+  onMount(()=>console.log("Simple"))
+
+  var clipboard = new ClipboardJS('.clip__');
+  clipboard.on('success', function (e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+   dispatch("copied")
+  e.clearSelection();
+});
+
+clipboard.on('error', function (e) {
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
+  dispatch("error")
+});
+</script>
+
+ 
+<div class="clip__ {className}" data-clipboard-text={value}>
+<slot>Just a filler</slot>
+</div>
