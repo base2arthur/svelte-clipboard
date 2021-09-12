8504b4de3401d612d27c7f57c5771c59424145f3
Arthur Marinis <a.marinis@base2services.com>
2021-09-12 11:40:13 +1000

    INitial deploy

---
+++ README.md
@@ -0,0 +1,20 @@
+ 
+
+# Svelte Component
+
+`A Svelte component template.`
+
+## Install
+
+```shell
+$ Copy. Don't clone
+```
+
+ 
+
+## Usage
+See src/Index.js
+ 
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/Index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import {  createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  onMount(()=>console.log("Simple"))
+</script>
+
+<style>
+  
+</style>
+
+<slot>Just a filler</slot>
